53983c4b25f014885f7dd58484e3a73e79d19e83
Chofo's Machine <diegovalenciavives@gmail.com>
2023-10-02 19:49:32 -0300

    transicion para trabajar con DRF - servicios acotados para solo mantener ordenes y usuarios - cambiado "ordenes" por "pedidos"

--- lib/pages/admin/menu_page.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MenuPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Menu Page'));
-  }
-}
--- lib/widgets/pedido_card.dart
+++ lib/widgets/pedido_card.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class OrdenCard extends StatelessWidget {
-  // final Orden orden;
-  final String? numeroMesa;
+class PedidoCard extends StatelessWidget {
+  final Map<String, dynamic> pedido;
   final Function()? onButtonPressed;
   final String? buttonLabel;
+  final Map<String, dynamic> productos = {
+    '1': {'nombre': 'Grande Mixto', 'cantidad': 2, 'precio_salida': 15.99},
+    '2': {'nombre': 'Grande Pollo', 'cantidad': 1, 'precio_salida': 9.99},
+  };
 
-  const OrdenCard({
-    // required this.orden,
-    this.numeroMesa,
+  PedidoCard({
+    required this.pedido,
     this.onButtonPressed,
     this.buttonLabel,
   });
 
   @override
   Widget build(BuildContext context) {
+    final String fTimestamp =
+        DateFormat('hh:mm').format(DateTime.parse(pedido['timestamp']));
     return AspectRatio(
       aspectRatio: 1 / 1,
       child: Stack(children: [
@@ -28,31 +33,38 @@ class OrdenCard extends StatelessWidget {
             children: [
               Row(
                 children: [
-                  // Text('#$orden.idOrden'),
+                  Text('#${pedido['id']}'),
                   Spacer(),
-                  Text('$numeroMesa ? "Mesa $numeroMesa" : "Para Llevar"}'),
+                  Text(pedido['mesa'] != null
+                      ? "Mesa ${pedido['mesa']}"
+                      : "Para Llevar"),
                 ],
               ),
               Row(
                 children: [
+                  Text('${pedido['estado']}'),
                   Spacer(),
-                  // Text('$orden.timeStamp'),
+                  Text('$fTimestamp'),
                 ],
               ),
-              // Expanded(
-              //   child: ListView.builder(
-              //     itemCount: orden.productos.length,
-              //     itemBuilder: (context, index) {
-              //       return Text(orden.productos[index].nombre);
-              //     },
-              //   ),
-              // ),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true, // this will make it as big as its content
+                  itemCount: productos.length,
+                  itemBuilder: (context, index) {
+                    String key = productos.keys.elementAt(index);
+                    return Text(
+                        "${productos[key]['cantidad']} x ${productos[key]['nombre']}");
+                  },
+                ),
+              ),
             ],
           ),
         ),
         onButtonPressed != null
             ? Positioned(
-                bottom: -8,
+                bottom: -3,
+                right: 1,
                 child: FilledButton(
                   style: ButtonStyle(
                       shape: MaterialStateProperty.all(RoundedRectangleBorder(
