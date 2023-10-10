305d6153950716fd92437771a9c78b25669028bd
Chofo's Machine <diegovalenciavives@gmail.com>
2023-10-10 16:17:05 -0300

    Corregido CRUD de Usuario - Agregado Drawer con datos de usuario y logout

--- lib/pages/cocina/cocina_page.dart
+++ lib/pages/cocina/cocina_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:jerupos/pages/login_page.dart';
-import 'package:jerupos/services/auth_service.dart';
 import 'package:jerupos/services/pedido_service.dart';
 import 'package:jerupos/widgets/pedido_card.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:jerupos/widgets/user_drawer.dart';
 
 class CocinaPage extends StatefulWidget {
   @override
@@ -11,14 +9,6 @@ class CocinaPage extends StatefulWidget {
 }
 
 class _CocinaPageState extends State<CocinaPage> {
-  Future<Map<String, dynamic>>? usuario;
-
-  @override
-  void initState() {
-    super.initState();
-    usuario = obtenerUsuario();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,47 +16,7 @@ class _CocinaPageState extends State<CocinaPage> {
         title: Text('JeruPOS'),
         backgroundColor: Colors.orange,
       ),
-      endDrawer: Drawer(
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView(children: [
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 0, 255, 145),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        radius: 40.0,
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Usuario",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ]),
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Logout'),
-              onTap: () {
-                AuthService.logout();
-                Navigator.of(context).pushAndRemoveUntil(
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                    (Route route) => false);
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: UserDrawer(),
       body: FutureBuilder(
         future: PedidoService.list(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -92,14 +42,5 @@ class _CocinaPageState extends State<CocinaPage> {
         },
       ),
     );
-  }
-
-  Future<Map<String, dynamic>> obtenerUsuario() async {
-    final String? token = await AuthService.getToken();
-    if (token != null) {
-      final Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
-      return decodedToken;
-    }
-    return {};
   }
 }
